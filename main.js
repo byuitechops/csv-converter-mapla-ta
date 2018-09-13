0157f8f0c69d149f63b9b10f8a8f9ac585465ece
Wilson <kilakal@byui.edu>
2018-09-13 15:38:03 -0600

    Code Review completed. Fixed most issues.

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-let zip = new JSZip();
-let check = false;
-let files = [];
-let invalidFiles = [];
-let validRowCount = 0;
-let invalidRowCount = 0;
-let downloadButton = document.querySelector('[id="download"]');
-let convertButton = document.getElementById('convert');
-let chooseFiles = document.getElementById('choose_files');
-let dropText = document.getElementById('dropText');
-let newData = [];
+const headersToCheck = ['Last', 'Given', 'Student ID', 'Total'];
+const headersToKeep = ['Student', 'Student ID', 'Section'];
+const headersToDelete = ['', 'Last', 'Given', 'Total'];
+let downloading,
+    files,
+    invalidFiles,
+    newFiles,
+    downloadButton = document.querySelector('[id="download"]'),
+    convertButton = document.getElementById('convert'),
+    chooseFiles = document.getElementById('choose_files'),
+    dropText = document.getElementById('dropText');
 
 /***************************************************
- *                   allowDownload()
+ *                    init()
  * 
- * Displays the download button which allows
- * the user to download a zip file containing the
- * converted CSV files. This function should only
- * be called once the data has been converted.
  * 
  * 
  * Return Type: void
  ***************************************************/
-function allowDownload() {
-    convertButton.style.display = 'none';
-    downloadButton.disabled = false;
-    downloadButton.style.display = 'inline-block';
-    dropText.innerHTML = 'Canvas CSV Download Ready';
+function init() {
+    newFiles = [];
+    files = [];
+    invalidFiles = [];
+    // Reset the text after downloading
+    document.getElementById('valid').innerHTML = '';
+    dropText.innerHTML = 'Drag and Drop CSV Files Here';
+    downloading = false;
+    // Fix buttons for after downloading
+    convertButton.style.display = 'inline-block';
+    convertButton.classList.add('disabled');
+    downloadButton.disabled = true;
+    downloadButton.style.display = 'none';
+    chooseFiles.style.display = 'block';
 }
 
 /***************************************************
@@ -34,51 +39,28 @@ function allowDownload() {
  * Modifies the data to conform to Canvas standards.
  * While converting, the function makes a loading
  * wheel appear. The modified data is stored in the
- * newData array.
+ * newFiles array.
  * 
  * 
  * Return Type: void
  ***************************************************/
-function modifyCSV(files) {
-    let content = document.getElementById('content');
-    let loader = document.getElementById('loader');
-    invalidFiles = [];
-    content.style.display = 'none';
-    chooseFiles.style.display = 'none';
-    document.getElementById('invalid_zone').style.display = 'none';
-    document.getElementById('invalid').innerHTML = '';
-    loader.style.display = 'inline-block';
+function modifyCSV() {
     files.forEach((file, i) => {
-        newData.push({
+        newFiles.push({
             fileName: file.fileName,
             data: []
         });
         file.data.forEach(student => {
-            let assignments = Object.keys(student);
-            let numKeys = assignments.length;
-            assignments = assignments.slice(4, numKeys - 1);
             if (student[''] !== 'Total Points') {
-                let newStudent = {
-                    Student: `${student.Last}, ${student.Given}`,
-                    'Student ID': student['Student ID'],
-                    Section: ''
-                };
-                assignments.forEach(assignment => {
-                    newStudent[assignment] = student[assignment];
+                student.Student = `${student.Last}, ${student.Given}`;
+                student.Section = '';
+                headersToDelete.forEach(header => {
+                    delete student[header];
                 });
-                newData[i].data.push(newStudent);
+                newFiles[i].data.push(student);
             }
         });
     });
-    /* This function runs instantaniously, however to give the impression
-    of conversion a loader will appear on the screen for 1-2 seconds. Then
-    the download will be allowed.
-    */
-    window.setTimeout(() => {
-        loader.style.display = 'none';
-        content.style.display = 'block';
-        allowDownload();
-    }, Math.floor(Math.random() * 2 * 1000 + 1000));
 }
 
 /***************************************************
@@ -108,15 +90,11 @@ function removeDragData(event) {
  * Return Type: bool
  ***************************************************/
 function checkForDuplicates(fileName) {
-    if (invalidFiles.find(file => {
-        return fileName === file;
-    })) {
+    if (invalidFiles.some(file => fileName === file)) {
         // The user is trying to upload a file that has already been uploaded. Stop them.
         return true;
     }
-    if (files.find(file => {
-        return fileName === file.fileName;
-    })) {
+    if (files.some(file => fileName === file.fileName)) {
         // The user is trying to upload a file that has already been uploaded. Stop them.
         return true;
     }
@@ -135,51 +113,83 @@ function checkForDuplicates(fileName) {
  * Return Type: Array/null
  ***************************************************/
 function validateCSV(data) {
-    return data.match(/,Last,Given,Student ID,.+?,Total/);
+    return headersToCheck.every(header => data.columns.includes(header));
 }
 
 /***************************************************
- *                makeValidTable()
+ *                   addToTable()
  * 
- * Creates/Modifies the valid CSV table. This
- * function is called when a file validates.
  * 
  * Return Type: void
  ***************************************************/
-function makeValidTable(file) {
-    if (files.length % 5 === 1 || validRowCount === 0) {
-        validRowCount++;
-        let tableRow = document.createElement('tr');
-        tableRow.id = `row${validRowCount}`;
-        document.getElementById('valid').appendChild(tableRow);
-
+function addToTable(fileName, tableId) {
+    let table = document.getElementById(tableId);
+    let lastRow = table.querySelector('tr:last-child');
+    if (!lastRow || lastRow.children.length === 5) {
+        lastRow = document.createElement('tr');
+        table.appendChild(lastRow);
     }
     let tableData = document.createElement('td');
-    let node = document.createTextNode(file.name);
+    let node = document.createTextNode(fileName);
     tableData.appendChild(node);
-    document.getElementById(`row${validRowCount}`).appendChild(tableData);
+    lastRow.appendChild(tableData);
 }
 
 /***************************************************
- *                makeInvalidTable()
+ *                  fileOnLoad()
  * 
- * Creates/Modifies the invalid CSV table. This
- * function is called when a file fails to validate.
  * 
  * Return Type: void
  ***************************************************/
-function makeInvalidTable(file) {
-    if (invalidFiles.length % 5 === 1 || invalidRowCount === 0) {
-        invalidRowCount++;
-        let tableRow = document.createElement('tr');
-        tableRow.id = `invRow${invalidRowCount}`;
-        document.getElementById('invalid').appendChild(tableRow);
+function fileOnLoad(event, fileName) {
+    if (checkForDuplicates(fileName)) {
+        return;
     }
-    let tableData = document.createElement('td');
-    let node = document.createTextNode(file.name);
-    tableData.appendChild(node);
-    document.getElementById(`invRow${invalidRowCount}`).appendChild(tableData);
-    document.getElementById('invalid_zone').style.display = 'block';
+    event.target.result = event.target.result.replace(/\ufeff/g, '');
+    let data = d3.csvParse(event.target.result);
+    if (validateCSV(data)) {
+        convertButton.classList.remove('disabled');
+        files.push({
+            fileName,
+            data
+        });
+        addToTable(fileName, 'valid');
+    } else {
+        invalidFiles.push(fileName);
+        addToTable(fileName, 'invalid');
+        document.getElementById('invalidMSG').innerHTML = `*Please check that the CSV file contains the following column headers:${headersToCheck.join(', ')}`;
+        document.getElementById('invalid_zone').style.display = 'block';
+    }
+}
+
+/***************************************************
+ *           makeTheUserWaitForNoReason()
+ * 
+ * The modifyCSV() function runs instantaniously,
+ * however to give the impression of conversion a 
+ * loader will appear on the screen for 1-2 seconds.
+ * Then the download will be allowed.
+ *       
+ * Return Type: void
+ ***************************************************/
+function makeTheUserWaitForNoReason() {
+    let content = document.getElementById('content');
+    let loader = document.getElementById('loader');
+    content.style.display = 'none';
+    loader.style.display = 'inline-block';
+    window.setTimeout(() => {
+        chooseFiles.style.display = 'none';
+        document.getElementById('invalid_zone').style.display = 'none';
+        document.getElementById('invalid').innerHTML = '';
+        loader.style.display = 'none';
+        content.style.display = 'block';
+        // Allow the download
+        downloading = true;
+        convertButton.style.display = 'none';
+        downloadButton.disabled = false;
+        downloadButton.style.display = 'inline-block';
+        dropText.innerHTML = 'Canvas CSV Download Ready';
+    }, Math.floor(Math.random() * 2 * 1000 + 1000));
 }
 
 /* -- Event Listeners -- */
@@ -194,27 +204,31 @@ function makeInvalidTable(file) {
  * has been placed the download starts.  
  ***************************************************/
 downloadButton.addEventListener('click', event => {
-    newData.forEach((element, i) => {
-        zip.file(element.fileName, d3.csvFormat(element.data));
-    });
-    zip.generateAsync({
-        type: 'blob'
-    }).then(blob => {
-        saveAs(blob, 'canvasCSV.zip');
-        newData = [];
-        files = [];
-        convertButton.classList.add('disabled');
-        convertButton.style.display = 'inline-block';
-        downloadButton.disabled = true;
-        downloadButton.style.display = 'none';
-        chooseFiles.style.display = 'block';
-        document.getElementById('valid').innerHTML = '';
-        dropText.innerHTML = 'Drag and Drop CSV Files Here';
-        check = false;
-    }, err => {
-        console.error(err);
-        alert('There was an error downloading the zip file containing the CSVs');
-    });
+    // If the user is converting only 1 file, don't zip it
+    if (newFiles.length === 1) {
+        let soloFile = newFiles[0];
+        let headers = headersToKeep.concat(Object.keys(soloFile.data[0]).filter(n => !headersToKeep.includes(n)));
+        soloFile.fileName = soloFile.fileName.slice(0, soloFile.fileName.length - 4) + '(Canvas Ready).csv';
+        download(d3.csvFormat(soloFile.data, headers), soloFile.fileName, 'text/plain');
+        init();
+    } else {
+        let zip = new JSZip();
+        let now = moment();
+        newFiles.forEach((newFile) => {
+            let headers = headersToKeep.concat(Object.keys(newFile.data[0]).filter(n => !headersToKeep.includes(n)));
+            newFile.fileName = newFile.fileName.slice(0, newFile.fileName.length - 4) + '(Canvas Ready).csv';
+            zip.file(newFile.fileName, d3.csvFormat(newFile.data, headers));
+        });
+        zip.generateAsync({
+            type: 'blob'
+        }).then(blob => {
+            saveAs(blob, `CanvasReadyCSVs(${now.format('YYYY[Y]-MM[M]-DD[D]-hh[h]-mm[m]-SS[s]')}).zip`);
+            init();
+        }, err => {
+            console.error(err);
+            alert('There was an error downloading the zip file containing the CSVs');
+        });
+    }
 });
 
 /***************************************************
@@ -230,23 +244,7 @@ document.getElementById('file').addEventListener('change', event => {
     let tempFiles = Array.from(event.target.files);
     tempFiles.forEach(file => {
         let fileReader = new FileReader();
-        fileReader.onload = (event) => {
-            if (checkForDuplicates(file.name)) {
-                return;
-            }
-            event.target.result = event.target.result.replace(/\ufeff/g, '');
-            if (validateCSV(event.target.result)) {
-                convertButton.classList.remove('disabled');
-                files.push({
-                    fileName: file.name,
-                    data: d3.csvParse(event.target.result)
-                });
-                makeValidTable(file);
-            } else {
-                invalidFiles.push(file.name);
-                makeInvalidTable(file);
-            }
-        };
+        fileReader.onload = (event) => fileOnLoad(event, file.name);
         fileReader.readAsText(file);
     });
 });
@@ -262,46 +260,23 @@ document.getElementById('file').addEventListener('change', event => {
  ***************************************************/
 document.getElementById('drop_zone').addEventListener('drop', event => {
     event.preventDefault();
-    if (!check) {
-        let downloadButton = document.querySelector('[id="download"]');
-        let convertButton = document.getElementById('convert');
-        downloadButton.disabled = true;
-        downloadButton.style.display = 'none';
-        if (event.dataTransfer.items) {
-            // Use DataTransferItemList interface to access the file(s)
-            for (let i = 0; i < event.dataTransfer.items.length; i++) {
-                // If dropped items aren't files, reject them
-                if (event.dataTransfer.items[i].kind === 'file') {
-                    let file = event.dataTransfer.items[i].getAsFile();
-                    if (file.name.match(/.csv$/)) {
-                        let reader = new FileReader();
-                        reader.onload = (event) => {
-                            if (checkForDuplicates(file.name)) {
-                                return;
-                            }
-                            event.target.result = event.target.result.replace(/\ufeff/g, '');
-                            if (validateCSV(event.target.result)) {
-                                convertButton.classList.remove('disabled');
-                                files.push({
-                                    fileName: file.name,
-                                    data: d3.csvParse(event.target.result)
-                                });
-                                makeValidTable(file);
-                            } else {
-                                invalidFiles.push(file.name);
-                                makeInvalidTable(file);
-                            }
-                        };
-                        reader.readAsText(file);
-                    } else {
-                        console.error('Unsupported file type!');
-                    }
+    // If the user tries to drop files after converting, don't do anything
+    if (downloading) return;
+    if (event.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (let i = 0; i < event.dataTransfer.items.length; i++) {
+            // If dropped items aren't files, reject them
+            if (event.dataTransfer.items[i].kind === 'file') {
+                let file = event.dataTransfer.items[i].getAsFile();
+                if (file.name.match(/.csv$/)) {
+                    let fileReader = new FileReader();
+                    fileReader.onload = (event) => fileOnLoad(event, file.name);
+                    fileReader.readAsText(file);
+                } else {
+                    console.error('Unsupported file type!');
                 }
             }
-        } else {
-            return;
         }
-
         // Pass event to removeDragData for cleanup
         removeDragData(event);
     }
@@ -320,8 +295,8 @@ window.addEventListener('drop', event => {
 // Checks if the files array's length is greater than 0 before allowing the modifyCSV function to run 
 document.getElementById('convert').addEventListener('click', event => {
     if (files.length > 0) {
-        check = true;
-        modifyCSV(files);
+        makeTheUserWaitForNoReason();
+        modifyCSV();
     }
 });
 
@@ -329,3 +304,10 @@ document.getElementById('convert').addEventListener('click', event => {
 document.getElementById('reset').addEventListener('click', event => {
     window.location.reload();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+init();
